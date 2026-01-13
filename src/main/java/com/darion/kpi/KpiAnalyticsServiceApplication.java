12d60963c85cf481ab5f99e39812b6e5b0575586
Darion <darionsayles06@gmail.com>
2026-01-13 09:23:19 -0500

    Initial project setup: Spring Boot backend and Elasticsearch via Docker

---
+++ src/main/java/com/darion/kpi/KpiAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.darion.kpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiAnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiAnalyticsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/darion/kpi/KpiAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darion.kpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiAnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
